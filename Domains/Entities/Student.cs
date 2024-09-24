d2fb95e61cdb3d623fc08f1705a3c372c95e0d89
maher omari <maher.omari44@yahoo.com>
2024-09-24 20:17:23 +0300

    edit

--- Domains/Entities/Student.cs
+++ Domains/Entities/Student.cs
@@ -11,5 +11,7 @@ namespace Domains.Entities
         public uint Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public string Address1 { get; set; }
     }
 }
