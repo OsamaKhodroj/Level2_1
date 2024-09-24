27fd8a8d890d77fa1479b0ab3b63aa65a7a70c54
Osama Khodrog <khodrog@hotmail.com>
2024-09-24 19:49:59 +0300

    add manager class

---
+++ Domains/Interfaces/IManager.cs
@@ -0,0 +1,15 @@
+﻿using Domains.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains.Interfaces
+{
+    public interface IManager
+    {
+        void Add();
+        List<Student> List();
+    }
+}
