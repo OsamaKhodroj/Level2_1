2ea05ebdfeaf0c1776cbff8b85d2695da4d5187e
Osama Khodrog <khodrog@hotmail.com>
2024-09-24 19:39:32 +0300

    this inital push code

---
+++ Level2_1/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Level2_1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
